76751fac3dd3d71e96919ac00ccfa7f255bfddbb
Allen T. Brown <allentbrown@gmail.com>
2015-09-17 15:26:21 -0400

    Edit Friends

--- app/src/main/java/com/allentbrown/ribbit/MainActivity.java
+++ app/src/main/java/com/allentbrown/ribbit/MainActivity.java
@@ -95,6 +95,10 @@ public class MainActivity extends FragmentActivity {
             ParseUser.logOut();
             navigateToLogin();
         }
+        else if (itemId == R.id.action_edit_friends) {
+            Intent intent = new Intent(this, EditFriendsActivity.class);
+            startActivity(intent);
+        }
 
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/com/allentbrown/ribbit/ParseConstants.java
@@ -0,0 +1,11 @@
+package com.allentbrown.ribbit;
+
+/**
+ * Created by abrown54 on 9/17/2015.
+ */
+public final class ParseConstants {
+    // Class name
+
+    // Field names
+    public static final String KEY_USERNAME = "username";
+}
